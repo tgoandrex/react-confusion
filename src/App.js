a595c152b02e39d1febc026e49bf9c2f89da1e08
Andrew Stapleton <tgoandrex@gmail.com>
2020-11-11 22:35:17 -0600

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Component } from 'react';
 import './App.css';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
+
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -11,6 +14,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
